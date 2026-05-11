5ce92c2fb13a7796af16dbbf5fc89127bced51c3
Devxtechnic <develope.genius@gmail.com>
2026-05-11 02:42:50 +0000

    home.html

--- django/firstproject/myapp/views.py
+++ django/firstproject/myapp/views.py
@@ -1,4 +1,6 @@
+from django.shortcuts import render
 from django.http import HttpResponse
+
 def home(request):
-    return HttpResponse("Hello, World!")
-# Create your views here.
+    return HttpResponse("Hello Students Updated")
+
